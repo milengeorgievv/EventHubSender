8d930198b046d9ee8b7e797de779e2fe21edca3f
Milen Georgiev <menkatageorgiev@abv.bg>
2020-10-15 10:29:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubsSender
+{
+    class Program
+    {
+        private const string connectionString = "";
+        private const string eventHubName = "myeventhub";
+        
+        static async Task Main()
+        {
+            await using (var producerClient = new EventHubProducerClient(connectionString, eventHubName))
+            {
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event")));
+
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published");
+            }
+        }
+    }
+}
